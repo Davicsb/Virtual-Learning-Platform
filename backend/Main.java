79288a01f16a37a5bc7bb1f2ce2cde2a4a62fe68
Bahamut <jvtv@ic.ufal.br>
2025-10-21 19:50:19 -0300

    mudando o sistema

--- backend/Main.java
+++
@@ -1,7 +0,0 @@
-import models.*; //importa o pacote todo da pasta models (todos os arquivos basicamente)
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- backend/models/U.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public enum U {
-    ADMIN, PROFESSOR, ALUNO
-}
