fd7efc1e5a4097b4a48cddcdb2546d94cecf168d
Bahamut <jvtv@ic.ufal.br>
2025-10-26 03:00:03 -0300

    finalizando autenticação

--- backend/src/main/java/com/example/AVA/tutors/TutorsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AVA.tutors;
-
-import com.example.AVA.tutors.model.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TutorsRepository extends JpaRepository<Tutor, Integer> {
-}
